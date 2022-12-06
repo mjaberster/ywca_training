d68cd989b4d61c79a6e828ff646ec000e9ab92ad
Marwan Jaber <mjaberster@gmail.com>
2022-12-06 08:51:35 +0200

    yargs

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,21 +5,27 @@ const { hideBin } = require('yargs/helpers')
 
 
 const data_location = './data'
+try {
+    const files = fs.readdirSync(data_location)
 
-const files = fs.readdirSync(data_location)
-files.forEach(f => {
-    const data = fs.readFileSync(`${data_location}/${f}`)
-    const student = JSON.parse(data)
-    console.log("---");
-    console.log(`Student ID: ${student.studentId}`)
-    console.log(`Student Name: ${student.studentName}`)
-    console.log(`Courses:`)
-    student.courses.forEach(c => {
-        console.log(` Course ID: ${c.courseId}`)
-        console.log(` Course Name: ${c.courseName}`)
-        console.log(` Paid: ${c.paid}`)
+    files.forEach(f => {
+        const data = fs.readFileSync(`${data_location}/${f}`)
+        const student = JSON.parse(data)
+        console.log("---");
+        console.log(`Student ID: ${student.studentId}`)
+        console.log(`Student Name: ${student.studentName}`)
+        console.log(`Courses:`)
+        student.courses.forEach(c => {
+            console.log(` Course ID: ${c.courseId}`)
+            console.log(` Course Name: ${c.courseName}`)
+            console.log(` Paid: ${c.paid}`)
+        })
     })
-})
+} catch (err) {
+    console.log('An error has occured while trying to read the directory, please try again later')
+} finally {
+    console.log('Finished')
+}
 
 
 // const registrationFiles = fs.readdirSync(registration_location)
